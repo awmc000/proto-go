6e6e56595727c198983052fa3053779d8675cbb4
Alex McColm <alex.mccolm@foreseeson.com>
2026-04-01 12:31:42 -0700

    Can respond to a request once

--- 1/1.cpp
+++ 1/1.cpp
@@ -49,11 +49,35 @@ void *get_in_addr(struct sockaddr *sa) {
 }
 
 void serve_client(int file_desc) {
-    char buf[512];
-    if (recv(file_desc, buf, 512, 0) == -1)
+
+    char buf[1024];
+    memset(buf, 0, 1024);
+
+    if (recv(file_desc, buf, 1024, 0) == -1) {
         perror("send");
-    std::cout << "got buf: " << buf << std::endl;
-    json request = json::parse(buf);
+    }
+
+    std::string buf_s{buf};
+
+    try {
+        json j_complete = json::parse(buf_s);
+        std::cout << std::setw(4) << j_complete << "\n\n";
+    } catch (nlohmann::json::parse_error) {
+        std::cout << "parse error" << std::endl;
+        send(file_desc, "BAD!", 5, 0);
+    }
+
+    using namespace nlohmann::literals;
+
+    json j_response = {
+        {"method", "isPrime"},
+        {"prime", false}
+    };
+
+    std::string buf_re = j_response.dump() + "\n";
+
+    send(file_desc, buf_re.c_str(), buf_re.length(), 0);
+
     close(file_desc);
 }
 
@@ -123,6 +147,8 @@ int main(void) {
 
     printf("server: waiting for connections...\n");
 
+    std::vector<std::thread> workers;
+
     while(1) {  // main accept() loop
         sin_size = sizeof their_addr;
         new_fd = accept(sockfd, (struct sockaddr *)&their_addr,
@@ -138,6 +164,8 @@ int main(void) {
         printf("server: got connection from %s\n", s);
 
         std::thread worker(serve_client, new_fd);
+        workers.push_back(std::move(worker));
+        // worker.detach();
     }
 
     return 0;
